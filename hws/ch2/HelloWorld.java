6cf02111b74b956049bdbb2b8b3b037436e7207e
Kevin Qian <yanxiang.qian@gmail.com>
2015-09-22 17:58:04 +0800

    ch2

---
+++ hws/ch2/HelloWorld.java
@@ -0,0 +1,9 @@
+/** A program to display the message
+ *  "Hello World!" on standard output.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}  // end of class HelloWorld
